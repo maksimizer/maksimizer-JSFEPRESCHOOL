394c033fdc4e1b67af8be818a4f9ae51f59bee66
maksimizer <maximorlov@mail.ru>
2022-01-29 23:14:34 +0300

    feat: make interactive menu interactive)

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,20 @@
 const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
+
+burger.addEventListener('click', toggleMenu);
+
 function toggleMenu() {
   burger.classList.toggle('open');
+  nav.classList.toggle('nav-open');
 }
-burger.addEventListener('click', toggleMenu);
+
+
+nav.addEventListener('click', closeMenu);
+function closeMenu (event) {
+    if (event.target.classList.contains('nav-link')) {
+        burger.classList.toggle('open');
+        nav.classList.toggle('nav-open');
+    }
+}
+
+
